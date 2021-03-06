27843261e88844bfc4975876d688bdbd730db0a0
Chinh Hoang <quangchinh11t1@gmail.com>
2021-03-05 17:04:14 -0800

    add ruby specs

--- specs/rails.js
+++ specs/rails.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 var __assign = (this && this.__assign) || function () {
     __assign = Object.assign || function(t) {
         for (var s, i = 1, n = arguments.length; i < n; i++) {
@@ -1496,3 +1497,57 @@ var completionSpec = {
   },
 };
 >>>>>>> 880aa33... feat: update prettierignore
+=======
+var completionSpec = {
+    name: "rails",
+    description: "Ruby on Rails deployment script",
+    options: [
+<<<<<<< HEAD
+    {
+        name: [ '-d,', '--no-docs' ],
+        description: 'Deploys rails without any documentation.'
+    },
+    {
+        name: [ '-C,', '--copy' ],
+        description: 'Deploys rails by copying instead of linking vendor libraries.'
+    },
+    {
+        name: [ '-F,', '--force-overwrite' ],
+        description: 'Overwrites all previously deployed files, if any.'
+    },
+    {
+        name: [ '-D,', '--database' ],
+        description: 'Specify the database to be used. Supported values are mysql, oracle, postgresql, sqlite3, frontbase and ibm_db'
+    },
+    {
+        name: [ '-I,', '--internal' ],
+        description: 'Options to be passed to the upstream rails script.'
+    },
+    ]
+}
+>>>>>>> 33fe90f... add rails
+=======
+        {
+            name: ["-d,", "--no-docs"],
+            description: "Deploys rails without any documentation.",
+        },
+        {
+            name: ["-C,", "--copy"],
+            description: "Deploys rails by copying instead of linking vendor libraries.",
+        },
+        {
+            name: ["-F,", "--force-overwrite"],
+            description: "Overwrites all previously deployed files, if any.",
+        },
+        {
+            name: ["-D,", "--database"],
+            description: "Specify the database to be used. Supported values are mysql, oracle, postgresql, sqlite3, frontbase and ibm_db",
+        },
+        {
+            name: ["-I,", "--internal"],
+            description: "Options to be passed to the upstream rails script.",
+        },
+    ],
+};
+
+>>>>>>> 11c51fe... feat: migrate to typescript
