5718eabbdc87536205cb573c774c72e89139c44c
Chinh Hoang <quangchinh11t1@gmail.com>
2021-03-15 19:36:48 +0000

    feat(docker): restart args

--- specs/docker.js
+++ specs/docker.js
@@ -2376,8 +2376,7 @@ var completionSpec = {
 		{ 
 			name: "restart",     
 			description: "Restart one or more containers", 
-			// TODO: Running containers
-			args: {},
+			args: containersArg,
 			options: [
 				{
 					"description": "Seconds to wait for stop before killing the container (default 10)",
