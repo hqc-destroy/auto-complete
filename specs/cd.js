47c443d0da3c9f0de3dd14f79e884814da049c9e
Chinh Hoang <quangchinh11t1@gmail.com>
2021-03-23 21:11:50 +0100

    feat: update README

--- specs/cd.js
+++ specs/cd.js
@@ -1,9 +1,13 @@
 var completionSpec = {
-    name: "cd",
-    description: "Change the shell working directory",
+    name: 'cd',
+    description: 'Change the shell working directory',
     args: {
-        template: "folders"
-    }
+        template: 'folders',
+    },
+    subcommands: [
+        { name: "dog" },
+        { name: "git" }
+    ]
 };
 
 // var completionSpec = {
