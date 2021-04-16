08b74d9fe08fdd3ca5c8aa7415725e6710f1c944
Chinh Hoang <quangchinh11t1@gmail.com>
2021-04-16 09:18:58 +0100

    feat: (kubectl) --kubeconfig generator

--- dev/kubectl.ts
+++ dev/kubectl.ts
@@ -1198,7 +1198,16 @@ export const completionSpec: Fig.Spec = {
       name: "config",
       description:
         'Modify kubeconfig files using subcommands like "kubectl config set current-context my-context"',
-      options: [],
+      options: [
+        {
+          name: "--kubeconfig",
+          insertValue: "--kubeconfig=",
+          args: {
+            name: "path",
+            template: "filepaths",
+          },
+        },
+      ],
       subcommands: [
         {
           name: "current-context",
