f1b3cecf6df7543064d5fa2c3278780666d857e4
Chinh Hoang <quangchinh11t1@gmail.com>
2021-05-05 13:02:34 -0700

    Update fig.d.ts

--- schemas/fig.d.ts
+++ schemas/fig.d.ts
@@ -629,7 +629,7 @@ declare namespace Fig {
 
   export interface Cache {
     /**
-     * Time to live for the cache in seconds
+     * Time to live for the cache in milliseconds
      *
      * @example
      * 3600
